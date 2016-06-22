7c419dc8ed11bd54661033a6054ea0c51423c622
Valentin Funk <funk.valentin@gmail.com>
2016-06-22 13:16:23 +0200

    Add Server Creation

--- index.js
+++ index.js
@@ -3,26 +3,65 @@ var readline = require('readline'),
   querystring = require('querystring'),
   request = require('request-promise'),
   dotenv = require('dotenv'),
+  _ = require('lodash'),
   fs = Promise.promisifyAll(require('fs'));
 
 require('dotenv').config();
 
 var Api = require('scaleway'),
     client = new Api({token: process.env['SCALEWAY_TOKEN']});
+Promise.promisifyAll(client);
 
-var data = {
-  name: 'c1',
-  commercial_type: 'VC1S',
-  organization: '<ORGANIZATION_ID>',
-  image: '<IMAGE_ID>',
-  tags: ['test', 'demo'],
-  bootscript: ''
-};
+function checkLogin() {
+  return request({
+    method: 'GET',
+    uri:"https://account.scaleway.com/tokens",
+    headers: {
+      'X-AUTH-TOKEN': process.env['SCALEWAY_TOKEN']
+    },
+    json: true
+  }).catch((error) => {
+    return Promise.reject("Authentication Failed: " + error.error.message);
+  });
+}
 
-client.get('/bootscripts', {}, function(err, res) {
-  console.log(err, res.body);
-})
-/*
-client.post('/servers', data, function(err, res) {
-  console.log(res.server);
-});*/
+checkLogin()
+.then(() => {
+  var data = {
+    commercial_type: 'VC1S',
+    image: '047f1372-3923-471f-82ca-5ff69dbaf0f7', // Ubuntu Xenial
+    bootscript: 'b1777a5a-6d40-4d3d-8904-756b45efe370', // Docker
+    organization: process.env['SCALEWAY_ORG']
+  };
+
+  var targetServers = [
+    {
+      tags: ['master', 'etcd'],
+      name: "ostrich-master-1"
+    },
+    {
+      tags: ['master', 'etcd'],
+      name: "ostrich-master-2"
+    },
+    {
+      tags: ['node', 'etcd'],
+      name: "ostrich-node-1"
+    },
+    {
+      tags: ['node'],
+      name: "ostrich-node-2"
+    }
+  ];
+
+  return Promise.map(targetServers, (server) => {
+    server.tags.push('AUTHORIZED_KEY=' + process.env['SSH_PUBLIC_KEY']);
+    var blueprint = _.extend({}, data, server);
+    return client.postAsync('/servers', blueprint);
+  });
+}).then(function(resolved) {
+  console.log(resolved);
+  var servers = _.map(resolved, 'server');
+  console.log(_.map(servers, 'public_ip'));
+}, function(error) {
+  console.log(error);
+});
